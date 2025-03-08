2637c01f2436aeff212c0d65cf6d902a15a37595
Artem Zhitkov <aptem_deadly@mail.ru>
2025-03-08 20:56:27 +0500

    создана модель "Payments"

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, Payments
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     exclude = ("password",)
+
+
+@admin.register(Payments)
+class PaymentsAdmin(admin.ModelAdmin):
+    fields = ("user", "paid_course", "paid_lesson", "payment_amount", "payment_method")
