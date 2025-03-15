d8c7c21646683f10c57500b6b09f570f82e2d7d5
Artem Zhitkov <aptem_deadly@mail.ru>
2025-03-15 22:40:32 +0500

    add urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import UserUpdateAPIView, PaymentsListApiView, UserRetrieveAPIView
+from users.views import (
+    UserUpdateAPIView,
+    PaymentsListApiView,
+    UserRetrieveAPIView,
+    UserCreateAPIView,
+    UserDestroyAPIView,
+)
 
 app_name = UsersConfig.name
 
@@ -9,4 +17,8 @@ urlpatterns = [
     path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user-detail"),
     path("<int:pk>/update/", UserUpdateAPIView.as_view(), name="user-update"),
     path("payments/", PaymentsListApiView.as_view(), name="payments-list"),
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    path("<int:pk>/delete/", UserDestroyAPIView.as_view(), name="delete"),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
