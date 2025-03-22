6ff58132c7d4f23be43397baf69fe2b47e34cdd2
Artem Zhitkov <aptem_deadly@mail.ru>
2025-03-22 16:02:36 +0500

    fixes

--- materials/validators.py
+++ materials/validators.py
@@ -9,9 +9,9 @@ class UrlValidator:
         self.field = field
 
     def __call__(self, value):
-        reg = re.compile("^(https?://)?(www\.)?youtube\.com/?$")
+        reg = re.compile("^(https?://)?(www\.)?youtube\.com/.+$")
         tmp_val = dict(value).get(self.field)
         if not bool(reg.match(tmp_val)):
             raise serializers.ValidationError(
-                f"Неверный URL-адрес. Пожалуйста, укажите правильный URL-адрес YouTube."
+                f"Неверный URL-адрес. Пожалуйста, укажите правильный URL-адрес YouTube. Нельзя размещать ссылки на сторонние образовательные платформы или личные сайты"
             )
