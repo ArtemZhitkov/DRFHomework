c829a8eb335bc6fad65fee3de6430c4a54298740
Artem Zhitkov <aptem_deadly@mail.ru>
2025-03-25 23:47:45 +0500

    add drf-yasg and STRIPE_API_KEY

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
     "django_filters",
     "rest_framework_simplejwt",
+    "drf_yasg",
+
     "users",
     "materials",
 ]
@@ -141,3 +144,5 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
